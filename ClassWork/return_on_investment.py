03ad80b7b3d064db366e145bf985aa307c2aeb29
Durodola Abolaji <142089766+TheYRSD@users.noreply.github.com>
2025-05-10 14:36:53 +0100

    Added a proper pseudocode

--- ClassWork/return_on_investment.py
+++ ClassWork/return_on_investment.py
@@ -1,3 +1,28 @@
+"""
+PROMPT your user to enter an amount to be invested
+STORE the entered integer as capital
+
+PROMPT your user to enter an the duration in years
+STORE the entered integer as number_of_years
+
+PROMPT your user to enter the interest rate in percentage
+STORE the entered integer as interest_rate
+
+
+DIVIDE the interest_rate by 100
+STORE the entered integer as interest_rate
+
+START a loop that increasing by 1 every LOOP and stops once its gets to the number_of_years
+	MULTIPLY the capital by interest_rate
+	STORE the result as profit
+	
+	ADD the profit and the capital
+	STORE the result as investment amount
+	PRINT the investment amount
+	SAVE the investment_amount as the capital 
+
+
+"""
 capital = int(input("Enter the Investment amount: ₦"))
 number_of_years = int(input("Enter the number of Years: ")) 
 interest_rate =int(input("Enter the interest rate: %"))
