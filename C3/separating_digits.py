36e9ae5c03a61566365ab4a7dd45e197d2c89755
Durodola Abolaji <142089766+TheYRSD@users.noreply.github.com>
2025-05-11 00:07:57 +0100

    Completed

--- C3/separating_digits.py
+++ C3/separating_digits.py
@@ -1,4 +1,22 @@
-digit = int(input("Enter the 5 digits number: "))
+"""
+PROMPT the user to enter a five digits number and store as number
+intialize divisor to 10000
 
-for digits in range(1000,0,*10):
-	print(digits)
+	For each repeat
+	false divide the num by divisor to store the rightmost digits
+	with the use of module store the remaining values by number modulus divisor
+	print the digit
+	DIVIDE the divisor by 10 and repeat
+
+
+"""
+
+
+
+number = int(input("Enter the 5 digits number: "))
+divisor = 10000
+for repeat in range(5):
+	digit = number//divisor
+	number = number%divisor
+	print(f'{digit}', end=' ')
+	divisor = divisor//10
