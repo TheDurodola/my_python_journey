ce7c6cc0d957d2cf421a23ad5a50770bfe445c39
Durodola Abolaji <142089766+TheYRSD@users.noreply.github.com>
2025-05-11 10:11:48 +0100

    Create turning_test.py

---
+++ C3/turning_test.py
@@ -0,0 +1,9 @@
+input("What is your problem? ")
+
+question = input("Have you had this problem before(yes or no)? ")
+
+
+if question == 'yes':
+	print("Well, you have it again.")
+else:
+	print("Well, you have it now")
