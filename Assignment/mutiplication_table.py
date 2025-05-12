37f8a0765a3330fa19dc3d1bddfac03607f28ea4
Durodola Abolaji <142089766+TheYRSD@users.noreply.github.com>
2025-05-12 21:35:59 +0100

    Added heading

--- Assignment/mutiplication_table.py
+++ Assignment/mutiplication_table.py
@@ -1,5 +1,9 @@
-for i in range(1,13):
-	for j in range(1,13):
-		print(i*j, end='\t')
-	print("")
 
+print("\t\t\tMULTIPLICATION TABLE")
+
+for i in range(1,10):
+	for j in range(1,10):
+		print(i*j, end='\t')
+	print(" ")
+
+ 
