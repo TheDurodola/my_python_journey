aa320315f7f886ad9dc63ad77854baa02596d886
Durodola Abolaji <142089766+TheYRSD@users.noreply.github.com>
2025-05-13 19:46:38 +0100

    Completed the formatting of the multiplication table

--- Assignment/mutiplication_table.py
+++ Assignment/mutiplication_table.py
@@ -1,9 +1,22 @@
 
-print("\t\t\tMULTIPLICATION TABLE")
+print("\t\t\t\tMULTIPLICATION TABLE")
+print("\t\t\t\t====================\n")
+
+
+for header in range(1,10):
+	print("       ",header, end='')
+
+
+print("\n===================================================================================")
+
 
 for i in range(1,10):
+	for x in range(1,10):
+		print("", end='')
+	print(i,end="||	 ")	
 	for j in range(1,10):
-		print(i*j, end='\t')
-	print(" ")
+
+		print(i*j, end=' \t    ')
+	print("    ")
 
  
