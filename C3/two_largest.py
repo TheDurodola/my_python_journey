7ed6458c853cd56c44f31a33cbd80370837dea47
Durodola Abolaji <142089766+TheYRSD@users.noreply.github.com>
2025-05-14 18:59:17 +0100

    Fix the bug with the second largest number

--- C3/two_largest.py
+++ C3/two_largest.py
@@ -1,16 +1,29 @@
 #Ex 3.16
 
 
-largest = 0
-second_largest =0
 
 
-for repeat in range(11):
-	number= int(input("Enter the number"))
-	if number>largest: 
-			second_largest=largest
-	#if second_largest>largest
-			largest=number
+first_number = int(input("Enter number 1: "))
 
-print(largest)
-print(second_largest)
+largest = first_number
+second_largest = 0
+
+
+for repeat in range(1,11):
+	number= int(input("Enter number: "))
+	if number>largest and number>second_largest: 
+			largest = number
+	if number>second_largest and number<largest:
+			second_largest=number
+	if largest==second_largest:
+		second_largest=number
+print(f"\nThe Largest integer is ",largest)
+print(f"The Second integer is ",second_largest)
+
+"""if student_score>highest_score and student_score>second_score:
+		highest_score = student_score
+		highest_name = student_name
+	if student_score>second_score and student_score<highest_score:
+		second_score = student_score
+		second_name = student_name
+"""
