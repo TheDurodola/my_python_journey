bd818b34ddee318678db93e3e62ab3ee9d3ebe1a
Durodola Abolaji <142089766+TheYRSD@users.noreply.github.com>
2025-05-14 06:16:49 +0100

    Finished up the asterisk

--- C3/asterisk_looping.py
+++ C3/asterisk_looping.py
@@ -11,9 +11,21 @@ for loop in range(11,0,-1):
 print()
 
 for loop in range(11):
-	for looping in range(loop):
+	for looping in range(loop+1):
+		print(' ', end='')
+	
+	for loop in range(loop,11):
+		print('*', end='')
+	print('')
+
+
+print()
+
+
+for loop in range(11):
+	for looping in range(loop,11):
 		print('', end=' ')
 	
-	for loop in range(11):
+	for loop in range(loop+1):
 		print('*', end='')
-	print(' ')
+	print('')
