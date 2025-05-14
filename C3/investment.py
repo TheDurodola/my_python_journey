72d70095c3a548e2eabdd5b2b91e3671e6b929e3
Durodola Abolaji <142089766+TheYRSD@users.noreply.github.com>
2025-05-14 12:42:36 +0100

    Increase readability by displaying the Year and putting everything in a tabular format

--- C3/investment.py
+++ C3/investment.py
@@ -1,9 +1,11 @@
 principal = 1000
 rate = 0.07
-
 year = 1
+
+print("YEAR	 ||	  AMOUNT")
+print("============================")
 for num in range(31):
 	amount = (1+rate)**year
 	amount = principal*amount
-	print(f"${amount}")
+	print(f"{num+1}	 ||	${amount:,.2f}")
 	year = year+1
