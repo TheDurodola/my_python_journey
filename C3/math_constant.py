6ddb6aeb1aef4cb61a14c73af773d5592ead7d0e
Durodola Abolaji <142089766+TheYRSD@users.noreply.github.com>
2025-05-14 18:58:10 +0100

    Renamed from math_constant to math_constant_e

--- C3/math_constant.py
+++
@@ -1,9 +0,0 @@
-import math
-
-euler_number = 1
-
-for repeating in range(1,11):
-	divisor = 1/math.factorial(repeating)
-	euler_number = euler_number+divisor
-
-print(f'The value of e the {repeating}th summation is {euler_number}')
