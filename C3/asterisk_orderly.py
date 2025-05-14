5c43464045fe161e9ddfc71b2c3b28182e09a2a7
Durodola Abolaji <142089766+TheYRSD@users.noreply.github.com>
2025-05-14 06:17:12 +0100

    First part of the triangle completed

---
+++ C3/asterisk_orderly.py
@@ -0,0 +1,13 @@
+NUMBER_OF_ASTERISK = 11
+
+for loop in range(1,NUMBER_OF_ASTERISK):
+	for looping in range(loop):
+		print('*', end='')
+	#print(" ")
+	for looping in range(loop,NUMBER_OF_ASTERISK):
+		print(' ', end='')
+	for looping in range(1):
+		print(' ')
+
+
+	
