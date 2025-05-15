02aa85c2d685ca6cd3c9a4f1d813509ac494cafc
Durodola Abolaji <142089766+TheYRSD@users.noreply.github.com>
2025-05-15 23:53:28 +0100

    Change the variable name divisor to fraction for readiabiility

--- C3/math_constant_e.py
+++ C3/math_constant_e.py
@@ -2,7 +2,8 @@ euler_number = 1
 denominator =1
 
 for repeating in range(2,11):
-	divisor = 1/denominator
-	euler_number = euler_number+divisor
+	fraction = 1/denominator
+	euler_number = euler_number+
 	denominator = denominator * repeating
+
 print(f'The value of e the {repeating+1}th summation is {euler_number}')
