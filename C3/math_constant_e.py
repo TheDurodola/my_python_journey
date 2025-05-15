ad5e67c503854aa5b44a4c7a5f46741168c53190
Durodola Abolaji <142089766+TheYRSD@users.noreply.github.com>
2025-05-15 09:24:34 +0100

    Adjusted the  display of the number of the summation to be more accurate

--- C3/math_constant_e.py
+++ C3/math_constant_e.py
@@ -5,4 +5,4 @@ for repeating in range(2,11):
 	divisor = 1/denominator
 	euler_number = euler_number+divisor
 	denominator = denominator * repeating
-print(f'The value of e the {repeating}th summation is {euler_number}')
+print(f'The value of e the {repeating+1}th summation is {euler_number}')
