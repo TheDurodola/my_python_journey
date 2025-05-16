b0724795e8e76ab5130e35272c9b74573f7dadc6
Durodola Abolaji <142089766+TheYRSD@users.noreply.github.com>
2025-05-16 12:50:29 +0100

    Update asterisk_orderly.py

--- C3/asterisk_orderly.py
+++ C3/asterisk_orderly.py
@@ -1,18 +1,11 @@
 #Ex 3.18
 
 
-NUMBER_OF_ASTERISK = 11
+i = 11
 
-for loop in range(1,NUMBER_OF_ASTERISK):
-	for looping in range(loop):
-		print('*', end='')
-	#print(" ")
-	for looping in range(loop,NUMBER_OF_ASTERISK):
-		print(' ', end='')
-	for loop in range(11,9,-1):
-		for looping in range(loop):
-			print('*',end='')
-	print(" ")
+for loop in range(1,i):
+
+	print(f"{"*" * loop:<10} {"*" * (1-loop)}")
 
 
 	
