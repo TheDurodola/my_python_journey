9a6d84b7e04eeba811ab28281c5c719091df8924
Durodola Abolaji <142089766+TheYRSD@users.noreply.github.com>
2025-05-16 12:28:26 +0100

    Formatting Update

--- C3/math_constant_pi.py
+++ C3/math_constant_pi.py
@@ -12,17 +12,18 @@ for num in range(100000000):
 
 	first_number = first_number + (fraction)
 	if round(first_number,2) == 3.14 and stopper<1:
-			print(f"At the term {num}, number = {first_number:.2f} ")
+			print(f"At the {num}th term, the number {first_number:.2f} is achieved")
 			stopper = stopper +1
 	if round(first_number,3) == 3.141 and stopper<2:
-			print(f"At the term {num}, number = {first_number:.3f} ")
+			print(f"At the {num}th term, the number {first_number:.3f}is achieved")
 			stopper= stopper + 1
 	if round(first_number,4) == 3.1415 and stopper<3:
-			print(f"At the term {num}, number = {first_number:.4f} ")
+			print(f"At the {num}th term, the number {first_number:.4f} is achieved")
 			stopper = stopper +1
 	if round(first_number,5) == 3.14159 and stopper<4:
-			print(f"At the term {num}, number = {first_number:.5f} ")
+			print(f"At the {num}th term, the number {first_number:.5f} is achieved", end="")
 			stopper = stopper +1
+			break
 
 	denominator = denominator +2
 	#print(f"{num+1}		||	{first_number:.5f}")
