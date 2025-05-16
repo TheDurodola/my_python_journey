ffedf8a3febc696603ed81a9239bf156bbdd916b
Durodola Abolaji <142089766+TheYRSD@users.noreply.github.com>
2025-05-16 12:28:05 +0100

    Added a mssing variable fraction to complete the code

--- C3/math_constant_e.py
+++ C3/math_constant_e.py
@@ -3,7 +3,7 @@ denominator =1
 
 for repeating in range(2,11):
 	fraction = 1/denominator
-	euler_number = euler_number+
+	euler_number = euler_number+ fraction
 	denominator = denominator * repeating
 
 print(f'The value of e the {repeating+1}th summation is {euler_number}')
