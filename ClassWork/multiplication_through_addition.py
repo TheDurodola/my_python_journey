5054035001f48bae608bc10a10076e81e32dc076
Durodola Abolaji <142089766+TheYRSD@users.noreply.github.com>
2025-05-16 20:35:50 +0100

    Update multiplication_through_addition.py

--- ClassWork/multiplication_through_addition.py
+++ ClassWork/multiplication_through_addition.py
@@ -2,13 +2,19 @@ def multiply(number1,number2):
 
 
 	number=0
-	for index in range(number2):
-		number = number + number1
-	return number
+	if number2<0:
+		number2 = - (number2)
+		for index in range(number2):
+			number = number + number1
+	
+	if number2>0:
+		return number
+	if number2<0:
+		return number
 
 
-number1 = int(input("Enter the first number: "))
-number2 = int(input("Enter the second number: "))
+number1 = float(input("Enter the first number: "))
+number2 = float(input("Enter the second number: "))
 
 print(f"\nThe multiplication of {number1} and {number2} is",multiply(number1,number2))
 
