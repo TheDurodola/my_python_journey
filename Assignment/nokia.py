4502c11337afc5d0672d7fa052e73142a97a4169
Durodola Abolaji <142089766+TheYRSD@users.noreply.github.com>
2025-05-16 20:35:42 +0100

    Create nokia.py

---
+++ Assignment/nokia.py
@@ -0,0 +1,22 @@
+loading_screen = """
+1) Turn-On
+0) Turn-Off"""
+print(loading_screen)
+navigate = int(input(""))
+
+if navigate==1:
+	print(main_men())
+
+
+def main_men():
+	main_menu = """
+1) Phone Book
+2) Messages	
+		
+
+
+
+
+
+
+	print	"""	
