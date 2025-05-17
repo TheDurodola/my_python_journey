3c56410693f1605f225ea1e5c2877d3f22bd167e
Durodola Abolaji <142089766+TheYRSD@users.noreply.github.com>
2025-05-17 02:49:18 +0100

    Edit the file to me able to make use of the function file

--- Assignment/nokia.py
+++ Assignment/nokia.py
@@ -1,22 +1,29 @@
-loading_screen = """
+import nokia_function
+
+counter =1
+while counter>0:
+
+	loading_screen = """ 	
+		NOKIA 3310
+
 1) Turn-On
-0) Turn-Off"""
-print(loading_screen)
-navigate = int(input(""))
+0) Turn-Off
 
-if navigate==1:
-	print(main_men())
-
-
-def main_men():
-	main_menu = """
-1) Phone Book
-2) Messages	
-		
+"""
+	print(loading_screen)
+	navigate = int(input("Enter a number to move: "))
+	if navigate==0:
+		counter=0
+	if navigate==1:
+		nokia_function.main_menu()		
 
 
 
 
 
 
-	print	"""	
+
+
+
+
+
