70410fa7bfeaecee5ef2d3334ac3b7831761e32d
Durodola Abolaji <142089766+TheYRSD@users.noreply.github.com>
2025-05-17 17:42:16 +0100

    Asterisk triangles on the same line completed

--- C3/asterisk_orderly.py
+++ C3/asterisk_orderly.py
@@ -1,12 +1,5 @@
 #Ex 3.18
 
 
-i = 11
-
-for loop in range(1,i):
-
-	print(f"{"*" * loop:<10} {"*" * (1-loop)}")
-
-
-	
-	
+for i in range(1, 11):
+	print(f'{'*' * i:<10} {'*' * (11 - i):<10} {' ' * i + '*' * (11 - i)} {' ' * (10 - i) + '*' * i}')
