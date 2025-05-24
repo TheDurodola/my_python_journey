6f94d443504e8680c1572b54e50cec6579010b64
Durodola Abolaji <142089766+TheDurodola@users.noreply.github.com>
2025-05-24 20:54:32 +0100

    Create bank_app.py

---
+++ Assignment/bank_app.py
@@ -0,0 +1,52 @@
+import bank_app_functions
+import random
+
+bank_database = []
+main_menu_loop_condition=0
+counter = 0
+
+
+
+
+while main_menu_loop_condition!=1:
+
+		main_menu_display = """
+\t\t\tWELCOME TO YRSD BANK
+
+1) Create Account
+2) Existing Account
+3) Menu
+0) Exit
+"""
+		print(main_menu_display)
+		main_menu_input = int(input("What would you like to do: "))
+		
+		
+		
+		match(main_menu_input):
+			
+			case 1:
+				bank_database.append(bank_app_functions.create_account())
+				print(f"\nWelcome {bank_database[counter][1]}, your account number is {bank_database[counter][0]}\n")
+				counter +=1
+			
+			case 2:
+				existing_account_number = int(input("Enter your 10-Digits account number: "))
+				current_account = bank_app_functions.get_existing_account(bank_database, existing_account_number)
+				print(current_account)
+				
+			case 3:
+				bank_app_functions.get_submenu(current_account)
+
+			case 0:
+				print("\nTHANK YOU FOR USING YRSD BANK")
+				main_menu_loop_condition=1
+		
+
+		
+
+
+			
+	
+
+
