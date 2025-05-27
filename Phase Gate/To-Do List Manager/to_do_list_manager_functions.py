4ed0b9facd5aac3aabea6fdb43af36896606955e
Durodola Abolaji <142089766+TheDurodola@users.noreply.github.com>
2025-05-27 12:49:57 +0100

    (3/4) functions for mark as completed done

--- Phase Gate/To-Do List Manager/to_do_list_manager_functions.py
+++ Phase Gate/To-Do List Manager/to_do_list_manager_functions.py
@@ -1,8 +1,25 @@
-def add_to_list():
-	add_task = input("Enter the task: ")
-	print("Task added!")
+def add_to_list(add_task):
+	print("Task Added!")
 	return add_task
 
-def view_all_tasks(list_of_task):
+def view_all_tasks(status_of_task, list_of_task):
+	for index, status in enumerate(status_of_task):
+		print(index+1, status, list_of_task[index])
+	
+def mark_task_as_completed(status_of_task, list_of_task):
 	for index, task in enumerate(list_of_task):
 			print(index+1, task)
+	user_input = int(input("Enter the number of the completed task: "))
+	user_input = user_input-1
+	status_of_task[user_input] = "[X]"	
+	return status_of_task
+
+	
+def delete_a_task(status,list_of_task):
+	for index, task in enumerate(list_of_task):
+			print(index+1, task)
+	user_input = int(input("Enter the number of your task displayed above: "))
+	user_input = user_input-1
+	print("Task Deleted!")
+	return list_of_task[user_input]
+	
