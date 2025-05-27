6b3c23c321154d0e85c69c71cd2579743dd71497
Durodola Abolaji <142089766+TheDurodola@users.noreply.github.com>
2025-05-27 14:01:26 +0100

    changed the .remove to .pop

--- Phase Gate/To-Do List Manager/to_do_list_manager_functions.py
+++ Phase Gate/To-Do List Manager/to_do_list_manager_functions.py
@@ -16,11 +16,11 @@ def mark_task_as_completed(status_of_task, list_of_task):
 
 	
 def delete_a_task(user_input, list_of_task):
-	list_of_task.remove(user_input)
+	list_of_task.pop(user_input)
 	return list_of_task
 
 def delete_status(user_input, status_of_task):
-	status_of_task.remove(user_input)
+	status_of_task.pop(user_input)
 	return status_of_task
 
 
