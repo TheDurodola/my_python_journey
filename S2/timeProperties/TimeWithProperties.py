019dd887a49b81f5f083b975e1c04b416600edb1
Durodola Abolaji <142089766+TheDurodola@users.noreply.github.com>
2025-07-20 20:17:46 +0100

    Create TimeWithProperties.py

---
+++ S2/timeProperties/TimeWithProperties.py
@@ -0,0 +1,42 @@
+
+class TimeWithProperties:
+    def __init__(self, seconds=0, minutes=0, hours=0):
+        self.seconds = seconds
+        self.minutes = minutes
+        self.hours = hours
+
+    @property
+    def seconds(self):
+        return self._seconds
+
+
+    @seconds.setter
+    def seconds(self, value):
+        if 0 > value > 59:
+            raise ValueError('seconds must be between 0 and 59')
+        self._seconds = value
+
+
+    @property
+    def minutes(self):
+        return self._minutes
+
+    @minutes.setter
+    def minutes(self, value):
+        if 0 > value > 59:
+            raise ValueError('minutes must be between 0 and 59')
+        self._minutes = value
+
+
+    @property
+    def hours(self):
+        return self._hours
+
+    @hours.setter
+    def hours(self, value):
+        if 0 > value > 23:
+            raise ValueError('hours must be between 0 and 23')
+        self._hours = value
+
+
+
