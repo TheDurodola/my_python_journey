370c5acc18f25d4203a665d2e485282301df9485
Durodola Abolaji <142089766+TheDurodola@users.noreply.github.com>
2025-07-23 03:57:44 +0100

    Add contact management methods to Phonebook
    Implemented methods for deleting, finding, and editing contacts in the Phonebook class. These additions enhance the functionality by allowing contact lookup by first name, last name, or phone number, and provide a way to remove or edit existing contacts.

--- S2/phonebookapp/phonebook.py
+++ S2/phonebookapp/phonebook.py
@@ -12,3 +12,32 @@ class Phonebook():
 
     def add_contact(self, firstName, lastName, phoneNumber):
         self.__database.append(Contact(firstName, lastName, phoneNumber))
+
+    def delete_contact(self, firstname, lastname, number):
+        for contact in self.__database:
+            if contact.get_firstname == firstname and contact.get_lastname == lastname and contact.get_phonenumber == number:
+                self.__database.remove(contact)
+                break
+
+    def find_contact_via_firstname(self, firstname):
+        for contact in self.__database:
+            if contact.get_firstname == firstname:
+                return contact.get_firstname, contact.get_lastname, contact.get_phonenumber
+        return None
+
+    def find_contact_via_lastname(self, lastname):
+        for contact in self.__database:
+            if contact.get_lastname == lastname:
+                return contact.get_firstname, contact.get_lastname, contact.get_phonenumber
+        return None
+
+    def find_contact_via_phonenumber(self, number):
+        for contact in self.__database:
+            if contact.get_phonenumber == number:
+                return contact.get_firstname, contact.get_lastname, contact.get_phonenumber
+        return None
+
+    def edit_contact(self, old_first_name, old_last_name, new_first_name, new_last_name, new_phone_number):
+        for contact in self.__database:
+            if contact.get_firstname == old_first_name:
+        pass
